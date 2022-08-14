a82b177ee4ed07b095a0826a5c2cdca141253d88
Durwankur <ndurwankur14@gmail.com>
2022-08-15 00:25:18 +0530

    Updated till 15 August 2022

---
+++ beginning/ty.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var waitGroup = sync.WaitGroup{}
+
+func bigFunction() {
+	time.Sleep(2 * time.Second)
+	fmt.Println("Operation done")
+	waitGroup.Done()
+}
+
+func main() {
+	for i := 0; i < 20; i++ {
+		waitGroup.Add(1)
+		go bigFunction()
+	}
+	waitGroup.Wait()
+}
+
+// best example of multithreading I have seen so far in golang
